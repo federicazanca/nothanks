a71abe3972c9795eba68b8ad54b5a8833a2d5468
federica <federicazanca8@gmail.com>
2024-04-12 22:05:49 +0100

    created player

---
+++ source/player.py
@@ -0,0 +1,11 @@
+"""Player class to define player's properties."""
+from game_state import GameState
+
+class Player():
+    def __init__(self):
+        self.chips = 0
+        self.cards = []
+        pass
+    def play(self, game_state:GameState):
+        pass
+
