11f6ee5da44f74a1e3f309732b18deaebe9e7157
federica <federicazanca8@gmail.com>
2024-04-12 22:07:08 +0100

    chips to player

--- source/player.py
+++ source/player.py
@@ -1,11 +1,12 @@
 """Player class to define player's properties."""
 from game_state import GameState
 
-class Player():
-    def __init__(self):
-        self.chips = 0
+class Player:
+    def __init__(self, chips):
+        self.chips = chips
         self.cards = []
         pass
+
     def play(self, game_state:GameState):
         pass
 
