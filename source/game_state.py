39fc9a901ab93ef1b19326ee4a23067b4645ddac
Angelo Rendina <angelo.rendina91@gmail.com>
2024-04-12 22:16:18 +0100

    data in gamestate

--- source/game_state.py
+++ source/game_state.py
@@ -1,3 +1,16 @@
+from player import Player
+import random
+
 class GameState:
     def __init__(self) -> None:
-        pass
+        self.players = [Player(11), 5]
+        self.pool = 0
+        self.deck = self._generate_deck()
+        self.card = self.deck.pop()
+        self.active_player_index = 0
+
+    @staticmethod
+    def _generate_deck():
+        deck = [i for i in range(3, 36)]
+        random.shuffle(deck)
+        return deck[:-9]
